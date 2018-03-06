bd1d58660c87226c1c8d9430dfc8f06992f247fe
marqs <mhiienka@niksula.hut.fi>
2018-03-06 23:52:10 +0200

    Add detection for 640x400 VGA Mode 13h

--- software/sys_controller/ossc/firmware.h
+++ software/sys_controller/ossc/firmware.h
@@ -24,7 +24,7 @@
 #include "sysconfig.h"
 
 #define FW_VER_MAJOR            0
-#define FW_VER_MINOR            80
+#define FW_VER_MINOR            81
 
 #ifdef ENABLE_AUDIO
 #define FW_SUFFIX1              "a"
